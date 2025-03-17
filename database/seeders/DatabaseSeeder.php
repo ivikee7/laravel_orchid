cb6e84e831269dea6c5c4aa830aecb7f6b4b381d
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-03-17 12:46:53 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'ivikee7@gmail.com',
-            'password'=> Hash::make('admin@123...!@'),
+            'password' => Hash::make('admin@123...!@'),
         ]);
     }
 }
