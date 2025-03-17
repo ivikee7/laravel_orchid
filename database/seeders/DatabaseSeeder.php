d1d6cfecf3251d22e5d70acdf43b42b790ec643a
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-03-17 10:08:35 +0530

    orched

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'ivikee7@gmail.com',
+            'password'=> Hash::make('admin@123...!@'),
         ]);
     }
 }
